981b9a7fa8ba1f2d1126e8d5edfeafb7056ca9ca
Marta Wojdat <marjow4@gmail.com>
2021-12-30 21:27:52 +0100

    Attempt to secure app #1

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -10,10 +10,20 @@ exports.add = async (req, res) => {
     const { title, author, email } = req.fields;
     const file = req.files.file;
 
-    const pattern = new RegExp(/(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/, 'g');
-    // nie wiem jak to dalej zrobić
-
     if(title && author && email && file) { // if fields are not empty...
+      
+      const textPattern = new RegExp(/(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/, 'g'); // zostawiam tak to avoid troubles
+
+      const titleMatched = title.match(textPattern).join('');
+      if(titleMatched.length < title.length) throw new Error('Invalid characters...');
+
+      const authorMatched = author.match(textPattern).join('');
+      if(authorMatched.length < author.length) throw new Error('Invalid characters...');
+
+      // kradzione z githuba, bo jestem zlym czlowiekiem wypelnionym zbyt duza iloscia carbsow
+      const emailPattern = new RegExp(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.([a-z]{1,6}))$/i);
+      const emailMatched = email.match(emailPattern).join('');
+      if(emailMatched.length < email.length) throw new Error('Invalid characters...');
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
       const fileExt = fileName.split('.').slice(-1)[0];
@@ -49,27 +59,26 @@ exports.loadAll = async (req, res) => {
 exports.vote = async (req, res) => {
 
   try {
-    const clientIp = requestIp.getClientIp(req);
-    let voter = await Voter.findOne({ user: clientIp });
-    if(!voter){
-      voter = new Voter({ user: clientIp, votes: [] });
-      await voter.save();
-    }
-
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
     if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
     else {
-      if(voter.votes.inclues(photoToUpdate)){
-        res.status(500).json(err);
+      const clientIp = requestIp.getClientIp(req);
+      let voter = await Voter.findOne({ user: clientIp });
+      if(!voter){
+        voter = new Voter({ user: clientIp, votes: [] });
+        await voter.save();
       } else {
-        photoToUpdate.votes++;
-        photoToUpdate.save();
-        voter.votes.push(photoToUpdate);
-        res.send({ message: 'OK' });
+        if(voter.votes.includes(photoToUpdate)){
+          throw new Error('You\'ve already voted for this picture!');
+        } else {
+          voter.votes.push(photoToUpdate);
+        }
       }
+      photoToUpdate.votes++;
+      photoToUpdate.save();
+      res.send({ message: 'OK' });
     }
   } catch(err) {
     res.status(500).json(err);
   }
-
 };
