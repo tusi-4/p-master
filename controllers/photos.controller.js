2376dea746cdf0442e9b76a65a1b436494f9f126
Marta Wojdat <marjow4@gmail.com>
2021-12-30 16:40:18 +0100

    Pretend being a hacker

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,4 +1,6 @@
 const Photo = require('../models/photo.model');
+const Voter = require('../models/voter.model');
+const requestIp = require('request-ip');
 
 /****** SUBMIT PHOTO ********/
 
@@ -8,9 +10,14 @@ exports.add = async (req, res) => {
     const { title, author, email } = req.fields;
     const file = req.files.file;
 
+    const pattern = new RegExp(/(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/, 'g');
+    // nie wiem jak to dalej zrobić
+
     if(title && author && email && file) { // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
+      const fileExt = fileName.split('.').slice(-1)[0];
+      if(fileExt != 'gif' && fileExt != 'jpg' && fileExt != 'png') throw new Error('Wrong input!');
       const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
       await newPhoto.save(); // ...save new photo in DB
       res.json(newPhoto);
@@ -42,12 +49,24 @@ exports.loadAll = async (req, res) => {
 exports.vote = async (req, res) => {
 
   try {
+    const clientIp = requestIp.getClientIp(req);
+    let voter = await Voter.findOne({ user: clientIp });
+    if(!voter){
+      voter = new Voter({ user: clientIp, votes: [] });
+      await voter.save();
+    }
+
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
     if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
     else {
-      photoToUpdate.votes++;
-      photoToUpdate.save();
-      res.send({ message: 'OK' });
+      if(voter.votes.inclues(photoToUpdate)){
+        res.status(500).json(err);
+      } else {
+        photoToUpdate.votes++;
+        photoToUpdate.save();
+        voter.votes.push(photoToUpdate);
+        res.send({ message: 'OK' });
+      }
     }
   } catch(err) {
     res.status(500).json(err);
--- models/Photo.model.js
+++ models/Photo.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const photoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
+  title: { type: String, required: true, maxlength: 25 },
+  author: { type: String, required: true, maxlength: 50 },
   email: { type: String, required: true },
   src: { type: String, required: true },
   votes: { type: Number, required: true },
